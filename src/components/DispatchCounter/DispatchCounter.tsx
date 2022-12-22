0cda5c2dea94798d7972aa4a87ca0037debb5989
Seppe Marien <seppe.marien@hotmail.be>
2022-12-22 22:13:03 +0100

    feat: first implementation of dispatchCounter

--- src/components/DispatchCounter/DispatchCounter.tsx
+++ src/components/DispatchCounter/DispatchCounter.tsx
@@ -1,27 +1,59 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 import { Container, Controls, Reset, CustomInput } from './Counter.styles';
 
-//Uee useReducer
-const intialCount = 12;
+enum CounterActions {
+  CALCULATE = 'CALCULATE',
+  RESET = 'RESET',
+}
+
+type CounterAction = {
+  type: CounterActions;
+  payload?: number;
+};
+
+type CounterState = {
+  count: number;
+};
+
+const initialState = 12;
+
+const counterReducer = (state: CounterState, action: CounterAction) => {
+  switch (action.type) {
+    case CounterActions.CALCULATE:
+      const newValue = state.count + (action?.payload || 0);
+      if (newValue % 11 === 0) return { count: 0 };
+      return { count: newValue };
+
+    case CounterActions.RESET:
+      return { count: initialState };
+    default:
+      return state;
+  }
+};
 
 const DispatchCounter: React.FC = () => {
-  const [count, setCount] = React.useState<number>(intialCount);
+  const [state, dispatch] = useReducer(counterReducer, { count: initialState });
+  const [freeFieldValue, setFreeFieldValue] = useState<string>();
+
+  const calculateValue = (number: number) => {
+    dispatch({ type: CounterActions.CALCULATE, payload: number });
+  };
 
   return (
     <Container>
       <div>
-        <p>{count}</p>
+        <p>{state.count}</p>
       </div>
       <Controls>
-        <button> -5 </button>
-        <button> -1 </button>
-        <button> +1 </button>
-        <button> +5 </button>
+        <button onClick={() => calculateValue(-5)}> -5 </button>
+        <button onClick={() => calculateValue(-1)}> -1 </button>
+        <button onClick={() => calculateValue(1)}> +1 </button>
+        <button onClick={() => calculateValue(5)}> +5 </button>
       </Controls>
-      <Reset>RESET</Reset>
+      <Reset onClick={() => dispatch({ type: CounterActions.RESET })}>RESET</Reset>
       <CustomInput>
-        <input type="number" />
-        <button>ADD</button>
+        <input type="number" onChange={(e) => setFreeFieldValue(e.target.value)} />
+        <button onClick={() => calculateValue(Number(freeFieldValue))}>ADD</button>
       </CustomInput>
     </Container>
   );
