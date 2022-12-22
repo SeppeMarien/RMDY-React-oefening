8c1a1955776e7b656eac5a47273bbfa98e35f4e4
Seppe Marien <seppe.marien@hotmail.be>
2022-12-22 22:15:04 +0100

    feat: interval

--- src/components/Interval/Interval.tsx
+++ src/components/Interval/Interval.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const Interval: React.FC = () => {
   const [seconds, setSeconds] = React.useState(0);
+  const intervalRef = useRef<NodeJS.Timer | null>(null);
 
   const secondsCounter = () => {
     //Add 1 second every second
+    setSeconds((prev) => ++prev);
   };
 
   React.useEffect(() => {
-    //Initiate interval
+    startTimer();
+
+    return () => {
+      stopTimer();
+    };
   }, []);
-  return <p style={{ textAlign: 'center' }}>{seconds} seconds passed</p>;
+
+  function startTimer() {
+    if (!!intervalRef.current) return;
+    const interval = setInterval(secondsCounter, 1000);
+    intervalRef.current = interval;
+  }
+
+  function stopTimer() {
+    if (!intervalRef.current) return;
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+  }
+
+  return (
+    <>
+      <p style={{ textAlign: 'center' }}>{seconds} seconds passed</p>
+      <div style={{ display: 'flex', justifyContent: 'center ' }}>
+        <button onClick={stopTimer}>stop</button>
+        <button onClick={startTimer}>start</button>
+      </div>
+    </>
+  );
 };
 
 export default Interval;
