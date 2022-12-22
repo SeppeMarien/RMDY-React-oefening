aedd8fc94092cfbf5415ccd5dffccf4904501864
Seppe Marien <seppe.marien@hotmail.be>
2022-12-22 22:11:06 +0100

    feat: Api users

--- src/components/Api/Api.service.ts
+++ src/components/Api/Api.service.ts
@@ -1,22 +1,33 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const ENDPOINT = 'https://randomuser.me/api?results=5'
+const ENDPOINT = 'https://randomuser.me/api?results=5';
 
 export interface User {
-    gender: string
-    name: {
-        title: string,
-        first: string,
-        last: string,
-    },
-    picture: {
-        thumbnail: string,
-    }
+  gender: string;
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+  picture: {
+    thumbnail: string;
+  };
 }
 
+interface Data {
+  info: any;
+  results: User[];
+}
+
+// EXTRA: ik zou naam in het meervoud zetten (je haalt meerdere users op)
 const GetRandomUser = async () => {
-    //
-}
+  const result = await axios.get<Data>(ENDPOINT);
+  if (result.status !== 200) {
+    console.error('Error occured while fetching users >>>> ', result.statusText);
+    return [];
+  }
 
+  return result.data.results;
+};
 
 export default GetRandomUser;
--- src/components/Api/Api.tsx
+++ src/components/Api/Api.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GetRandomUser, { User } from './Api.service';
 import { Container, DataList, UserContainer } from './Api.styles';
 
 const Api: React.FC = () => {
   const [data, setData] = React.useState([] as Array<User>);
+  const [loading, setLoading] = useState(true);
 
-  React.useEffect(() => {}, []);
+  React.useEffect(() => {
+    const fetchUsers = async () => {
+      const users = await GetRandomUser();
+      setData(users);
+      setLoading(false);
+    };
+
+    fetchUsers();
+  }, []);
+
+  if (loading) return <p>Fetching users...</p>;
+  if (data.length === 0) return <p>No data found...</p>;
 
   return (
     <Container>
-      <DataList>{data.length > 0 ? 'Show userlist here' : <p>Fetching users...</p>}</DataList>
+      <DataList>
+        {data.map((user) => (
+          <UserContainer key={`${user.picture.thumbnail}`} isFemale={user.gender === 'female'}>
+            <img src={user.picture.thumbnail} alt="thumbnailPicture" />
+            <p>{`${user.name.title} ${user.name.last} ${user.name.first}`}</p>
+            <p>{user.gender}</p>
+          </UserContainer>
+        ))}
+      </DataList>
     </Container>
   );
 };
