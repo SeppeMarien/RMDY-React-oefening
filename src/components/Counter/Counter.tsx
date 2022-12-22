6a85873e209925821b78155092a78b5e998a38fe
Seppe Marien <seppe.marien@hotmail.be>
2022-12-22 22:08:10 +0100

    feat: implement counter exercise

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,34 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Container, Controls, Reset, CustomInput } from './Counter.styles';
-
 
 const intialCount = 12;
 
 const Counter: React.FC = () => {
+  const [count, setCount] = React.useState<number>(intialCount);
+  const [freeFieldValue, setFreeFieldValue] = useState<string>();
 
-    const [count, setCount] = React.useState<number>(intialCount)
+  const calculateValue = (number: number) => {
+    setCount((prev) => {
+      const newValue = prev + number;
+      if (newValue % 11 === 0) return 0;
+      return newValue;
+    });
+  };
 
-    return (
-        <Container>
-            <div>
-                <p>{count}</p>
-            </div>
-            <Controls>
-                <button> -5 </button>
-                <button> -1 </button>
-                <button> +1 </button>
-                <button> +5 </button>
-            </Controls>
-            <Reset>RESET</Reset>
-            <CustomInput>
-                <input type='number'/>
-                <button>ADD</button>
-            </CustomInput>
-        </Container>
-    )
-}
-
+  return (
+    <Container>
+      <div>
+        <p>{count}</p>
+      </div>
+      <Controls>
+        <button onClick={() => calculateValue(-5)}> -5 </button>
+        <button onClick={() => calculateValue(-1)}> -1 </button>
+        <button onClick={() => calculateValue(1)}> +1 </button>
+        <button onClick={() => calculateValue(5)}> +5 </button>
+      </Controls>
+      <Reset onClick={() => setCount(intialCount)}>RESET</Reset>
+      <CustomInput>
+        <input type="number" onChange={(e) => setFreeFieldValue(e.target.value)} />
+        <button onClick={() => calculateValue(Number(freeFieldValue))}>ADD</button>
+      </CustomInput>
+    </Container>
+  );
+};
 
 export default Counter;
-
-
